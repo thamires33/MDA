242dc864809de8046d62065981afec21ff3940c9
thamires33 <http.thami@gmail.com>
2023-08-28 17:06:40 -0300

    App com chatbot

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
 import cv2
 from cvzone.PoseModule import PoseDetector
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+browser.get("https://web.whatsapp.com/send/?phone=5511937218726&text&type=phone_number&app_absent=0") #https://web.whatsapp.com/send/?phone=5511976492228&text&type=phone_number&app_absent=0
 
 ##########   VIDEO   ##########
 cap = cv2.VideoCapture(0)
@@ -9,8 +14,13 @@ cap = cv2.VideoCapture(0)
 
 ##########   DETECTORS   ##########
 bodyDetector = PoseDetector()
-def SendAlert(text):
-    print(text)
+def SendAlert():
+    browser.find_element(By.XPATH, "//div[@class='to2l77zo gfz4du6o ag5g9lrv bze30y65 kao4egtt']").click()
+    browser.find_element(By.XPATH, "//div[@class='to2l77zo gfz4du6o ag5g9lrv bze30y65 kao4egtt']").send_keys("""Atenção, temos uma intercorrência no carro
+                                                                                                             Nº7854 com destino a estação Perus""")
+    time.sleep(5)
+    browser.find_element(By.XPATH, "//button[@class='tvf2evcx oq44ahr5 lb5m6g5c svlsagor p2rjqpw5 epia9gcq']").click()
+    time.sleep(3)
 ##########   PROCESSING   ##########
 while True:
     success, img = cap.read()
@@ -36,9 +46,9 @@ while True:
 
     elif  cv2.waitKey(1) & 0xFF == ord('s'):
         if 100 >= angle >= 40:
-            SendAlert("Sentado")
+            SendAlert()
         else:
-            SendAlert("Em pé")
+            pass
 
     else:
         continue
